fdd930dc4f5fcd4ced1d009337205c3ecba9c092
進藤雄太朗 <yutaroshindo@shindouyuutaakiranoMacBook-Pro.local>
2022-12-18 12:57:18 +0900

    20221218_4

--- middleware.js
+++ middleware.js
@@ -1,25 +1,53 @@
-import { Buffer } from 'buffer';
-//window.Buffer = Buffer;
+// import { Buffer } from 'buffer';
+// //window.Buffer = Buffer;
+// import { NextRequest, NextResponse } from 'next/server'
+
+// export const config = {
+//   matcher: ['/work'],
+// }
+
+// export function middleware(req) {
+//   const basicAuth = req.headers.get('authorization')
+//   const url = req.nextUrl
+  
+//     if (basicAuth) {
+//       const auth = basicAuth.split(' ')[1]
+//       const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
+//       if (user === 'do-portfolio' && pwd === 'aser128kA9Zhn') {
+//         return NextResponse.next()
+//       }
+//     }
+  
+//   url.pathname = '/api/auth'
+
+//   return NextResponse.rewrite(url)
+// }
+
+
+import { next } from "@vercel/edge";
 import { NextRequest, NextResponse } from 'next/server'
-
 export const config = {
-  matcher: ['/work'],
-}
+  matcher: "/work",
+};
 
-export function middleware(req) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
-  
-    if (basicAuth) {
-      const auth = basicAuth.split(' ')[1]
-      const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+export default function middleware(request) {
+  const authorizationHeader = request.headers.get("authorization");
+  const url = request.nextUrl
 
-      if (user === 'do-portfolio' && pwd === 'aser128kA9Zhn') {
-        return NextResponse.next()
-      }
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(" ")[1];
+    const [user, pwd] = Buffer.from(basicAuth, 'base64').toString().split(':')
+    //const [user, password] = atob(basicAuth).toString().split(":");
+
+    if (user === 'do-portfolio' && pwd=== 'aser128kA9Zhn') {
+      // return next();
+      return NextResponse.next()
     }
-  
+  }
   url.pathname = '/api/auth'
-
   return NextResponse.rewrite(url)
+  //return next();
+
 }
+
--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,14 +2,25 @@
 
 
 
-// import type { NextApiRequest, NextApiResponse } from 'next'
+import{ NextApiRequest, NextApiResponse } from 'next'
 
-// export default function handler(_, res) {
-//   res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"')
-//   res.statusCode = 401
-//   res.end(`Auth Required.`)
-// }
+export default function handler(_, res) {
+  res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"')
+  res.statusCode = 401
+  res.end(`Auth Required.`)
+}
 
-import  {  createApiPage  }  from  'nextjs-basic-auth-middleware' 
+// import  {  createApiPage  }  from  'nextjs-basic-auth-middleware' 
 
-export  default  createApiPage ( )
+// export  default  createApiPage ( )
+
+
+
+// export function next(init){
+//     const headers = new Headers(init?.headers ?? {});
+//     headers.set('x-middleware-next', '1');
+//     return new Response(null, {
+//       ...init,
+//       headers,
+//     });
+//   }
