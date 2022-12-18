e97fe386ba9ce68f765801977ae77eceeba0e966
進藤雄太朗 <yutaroshindo@shindouyuutaakiranoMacBook-Pro.local>
2022-12-18 16:58:09 +0900

    20221218_6

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   images: {
     domains: ['images.microcms-assets.io'],
   },
@@ -11,22 +10,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.microcms-assets.io',
         port: '',
-        // pathname: '/account123/**',
+        
       },
     ],
   },
-  // experimental: {
-  //   optimizeFonts: true,
-  // },
-  //presets: ["next/babel"],
-  // swcMinify: false ,
+  swcMinify: false ,
   reactStrictMode: false,
-  // env: {
-  //   ENABLE_BASIC_AUTH: process.env.ENABLE_BASIC_AUTH,
-  //   BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
-  //   BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
-  // },
-
 }
 
 module.exports = nextConfig
