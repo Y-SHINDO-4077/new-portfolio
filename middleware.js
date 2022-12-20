40ce1492ab5ddda80cb4470bb3cf4f2ebdefb945
進藤雄太朗 <yutaroshindo@shindouyuutaakiranoMacBook-Pro.local>
2022-12-20 22:39:20 +0900

    20221220_3

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse} from 'next/server'
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/','/work','/work/*','/me','/other'],
 }
 
 export function middleware(req) {
