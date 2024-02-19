cdf1dd2f760c72486a77738460106b117ae916a7
進藤雄太朗 <nihonshishinyu1994@gmail.com>
2024-02-19 16:19:39 +0900

    fix:いったんconvert-bodyを編集、まだ直すところあり

--- components/convert-body.js
+++ components/convert-body.js
@@ -6,19 +6,25 @@ export default function ConvertBody({ contentHTML }) {
 			if (node.name === "img") {
 				const { src, alt, width, height } = node.attribs;
 				return (
-                    <Image
-                        src={src}
-                        width={width}
-                        height={height}
-                        alt={alt}
-                        sizes="(min-width: 768px) 768px, 100vw"
-                        style={{
-                            width: "100%",
-                            height: "auto"
-                        }} />
-                );
+					<Image
+						src={src}
+						width={width}
+						height={height}
+						alt={alt}
+						sizes="(min-width: 768px) 768px, 100vw"
+						style={{
+							width: "100%",
+							height: "auto",
+						}}
+						className="mt-8"
+					/>
+				);
 			}
 		},
 	});
-	return <>{contentReact}</>;
+	return (
+		<>
+			<div className="mt-8 text-2xl md:text-4xl">{contentReact}</div>
+		</>
+	);
 }
