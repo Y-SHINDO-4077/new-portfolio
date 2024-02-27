cea75722d26bc02a7b8b70f5bdc8a8c5875d3800
進藤雄太朗 <nihonshishinyu1994@gmail.com>
2024-02-27 22:02:52 +0900

    fix:404ページ

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,18 @@
 import Meta from "components/meta";
 import Container from "components/container";
 import Hero from "components/hero";
+import READMORE from "components/readmore";
 
 export default function custom404() {
 	return (
 		<Container>
 			<Meta pageTitle="404 - Page not found" />
-			<Hero title="404" subtitle="ページが見つかりません" />
+			<div className="py-20">
+				<Hero title="404" subtitle="ページが見つかりません" />
+				<div className="!mx-auto mt-16 flex w-fit pb-16">
+					<READMORE url="/" title="BACK TO TOP" />
+				</div>
+			</div>
 		</Container>
 	);
 }
