5134194f9b1012a265a69896b2f59cdf626fac2f
進藤雄太朗 <nihonshishinyu1994@gmail.com>
2024-02-29 20:27:58 +0900

    fix:カテゴリ絞り込みScrollTrigger

--- pages/work/category/[slug].js
+++ pages/work/category/[slug].js
@@ -7,7 +7,9 @@ import { getAllCategories, getAllPostsByCategory } from "lib/api";
 import Posts from "components/posts";
 import { getPlaiceholder } from "plaiceholder";
 import { eyecatchLocal } from "lib/constants";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export async function getStaticPaths() {
 	const allCats = await getAllCategories();
@@ -50,100 +52,144 @@ export default function Work({ name, posts, categories, ja_name }) {
 	const closeHandler = () => {
 		dialog.current.close();
 	};
+	const filterButton = useRef();
+	const workArea = useRef();
+	gsap.registerPlugin(ScrollTrigger);
+	useEffect(() => {
+		const mq = window.matchMedia("(max-width: 768px)");
+
+		const handleResize = () => {
+			if (mq.matches && filterButton.current && workArea.current) {
+				gsap.to(filterButton.current, {
+					scrollTrigger: {
+						trigger: workArea.current,
+						start: "bottom bottom",
+						toggleActions: "play none none reverse",
+						scrub: true,
+						onEnter: () => {
+							gsap.to(filterButton.current, { opacity: 0, display: "none", duration: 0.5 });
+						},
+						onLeave: () => {
+							gsap.to(filterButton.current, { opacity: 1, display: "flex", duration: 0.5 });
+						},
+						onEnterBack: () => {
+							gsap.to(filterButton.current, { opacity: 0, display: "none", duration: 0.5 });
+						},
+						onLeaveBack: () => {
+							gsap.to(filterButton.current, { opacity: 1, display: "flex", duration: 0.5 });
+						},
+					},
+				});
+			} else {
+				ScrollTrigger.refresh();
+			}
+		};
+
+		window.addEventListener("resize", handleResize);
+		handleResize();
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	return (
-		<div className="dark:bg-darkBaige">
-			<Container>
-				<Meta pageTitle={`${name} 実績カテゴリ | WORKS | DO`} pageDesc={`実績カテゴリ${name}`} />
-				<section className="relative py-20 dark:bg-darkBaige md:pt-16 ">
-					<Hero title="WORKS" subtitle={`${name}`} />
-					<p className="mt-8 text-center text-sm md:text-base">{ja_name}</p>
-					<button
-						id="showModal"
-						className="fixed bottom-0 left-0 z-10 inline-flex h-8 w-screen items-center justify-center bg-beige py-8 text-2xl font-bold underline dark:bg-black dark:text-white md:hidden"
-						onClick={openHandler}
-						aria-labelledby="modal-title"
-						aria-describedby="modal-description"
-					>
-						カテゴリで絞り込む
-					</button>
-					<dialog
-						id="modal"
-						className="relative px-12 text-center text-sm dark:bg-black md:hidden"
-						ref={dialog}
-						onClick={closeHandler}
-					>
-						<button
-							className="absolute right-4 top-4 text-3xl font-bold dark:text-white"
+		<>
+			<button
+				id="showModal"
+				className="fixed bottom-0 left-0 z-10 inline-flex h-8 w-screen items-center justify-center bg-beige py-8 text-2xl font-bold underline dark:bg-black dark:text-white md:hidden"
+				onClick={openHandler}
+				aria-labelledby="modal-title"
+				aria-describedby="modal-description"
+				ref={filterButton}
+			>
+				カテゴリで絞り込む
+			</button>
+			<div className="dark:bg-darkBaige" ref={workArea}>
+				<Container>
+					<Meta pageTitle={`${name} 実績カテゴリ | WORKS | DO`} pageDesc={`実績カテゴリ${name}`} />
+					<section className="relative py-20 dark:bg-darkBaige md:pt-16 ">
+						<Hero title="WORKS" subtitle={`${name}`} />
+						<p className="mt-8 text-center text-sm md:text-base">{ja_name}</p>
+
+						<dialog
+							id="modal"
+							className="relative px-12 text-center text-sm dark:bg-black md:hidden"
+							ref={dialog}
 							onClick={closeHandler}
 						>
-							<span>×</span>
-						</button>
-						<div
-							onClick={(e) => {
-								e.stopPropagation();
-							}}
-						>
-							<span
-								className="block pt-14 text-3xl font-bold dark:text-white"
-								id="modal-modal-title"
+							<button
+								className="absolute right-4 top-4 text-3xl font-bold dark:text-white"
+								onClick={closeHandler}
 							>
-								カテゴリで絞り込む
-							</span>
-							<ul
-								className="my-12 flex flex-col items-center justify-center gap-4 sm:flex-wrap md:flex-row md:gap-8"
-								id="modal-description"
+								<span>×</span>
+							</button>
+							<div
+								onClick={(e) => {
+									e.stopPropagation();
+								}}
 							>
-								<li>
-									<Link
-										href="/work/"
-										className="flex size-full overflow-hidden bg-gradient-to-l from-black/50 from-50% to-0% bg-[length:200%_4px] bg-[bottom_0_left_200%] bg-repeat-x font-futura text-2xl transition-all duration-300 ease-linear hover:bg-[bottom_0_left_100%] dark:from-white/50  dark:text-white md:text-3xl"
-									>
-										ALL
-									</Link>
-								</li>
-								{categories.map((categories) => (
-									<li key={categories.id}>
+								<span
+									className="block pt-14 text-3xl font-bold dark:text-white"
+									id="modal-modal-title"
+								>
+									カテゴリで絞り込む
+								</span>
+								<ul
+									className="my-12 flex flex-col items-center justify-center gap-4 sm:flex-wrap md:flex-row md:gap-8"
+									id="modal-description"
+								>
+									<li>
 										<Link
-											href={`/work/category/${categories.slug}`}
+											href="/work/"
 											className="flex size-full overflow-hidden bg-gradient-to-l from-black/50 from-50% to-0% bg-[length:200%_4px] bg-[bottom_0_left_200%] bg-repeat-x font-futura text-2xl transition-all duration-300 ease-linear hover:bg-[bottom_0_left_100%] dark:from-white/50  dark:text-white md:text-3xl"
 										>
-											{categories.name}{" "}
+											ALL
 										</Link>
 									</li>
-								))}
-							</ul>
-						</div>
-					</dialog>
-					<ul className="my-16  hidden items-center justify-center gap-4 sm:flex-wrap md:flex md:flex-row md:gap-8">
-						<li>
-							<Link
-								href="/work/"
-								className="flex size-full overflow-hidden bg-gradient-to-l from-black/50 from-50% to-0% bg-[length:200%_4px] bg-[bottom_0_left_200%] bg-repeat-x font-futura text-2xl transition-all duration-300 ease-linear hover:bg-[bottom_0_left_100%] dark:from-white/50  dark:text-white md:text-3xl"
-							>
-								ALL
-							</Link>
-						</li>
-						{categories.map((categories) => (
-							<li key={categories.id}>
+									{categories.map((categories) => (
+										<li key={categories.id}>
+											<Link
+												href={`/work/category/${categories.slug}`}
+												className="flex size-full overflow-hidden bg-gradient-to-l from-black/50 from-50% to-0% bg-[length:200%_4px] bg-[bottom_0_left_200%] bg-repeat-x font-futura text-2xl transition-all duration-300 ease-linear hover:bg-[bottom_0_left_100%] dark:from-white/50  dark:text-white md:text-3xl"
+											>
+												{categories.name}{" "}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</div>
+						</dialog>
+						<ul className="my-16  hidden items-center justify-center gap-4 sm:flex-wrap md:flex md:flex-row md:gap-8">
+							<li>
 								<Link
-									href={`/work/category/${categories.slug}`}
+									href="/work/"
 									className="flex size-full overflow-hidden bg-gradient-to-l from-black/50 from-50% to-0% bg-[length:200%_4px] bg-[bottom_0_left_200%] bg-repeat-x font-futura text-2xl transition-all duration-300 ease-linear hover:bg-[bottom_0_left_100%] dark:from-white/50  dark:text-white md:text-3xl"
 								>
-									{categories.name}{" "}
+									ALL
 								</Link>
 							</li>
-						))}
-					</ul>
-					{posts.length === 0 ? (
-						<div className="text-center">
-							<p>記事がありません。</p>
-						</div>
-					) : (
-						<Posts posts={posts} />
-					)}
-				</section>
-			</Container>
-		</div>
+							{categories.map((categories) => (
+								<li key={categories.id}>
+									<Link
+										href={`/work/category/${categories.slug}`}
+										className="flex size-full overflow-hidden bg-gradient-to-l from-black/50 from-50% to-0% bg-[length:200%_4px] bg-[bottom_0_left_200%] bg-repeat-x font-futura text-2xl transition-all duration-300 ease-linear hover:bg-[bottom_0_left_100%] dark:from-white/50  dark:text-white md:text-3xl"
+									>
+										{categories.name}{" "}
+									</Link>
+								</li>
+							))}
+						</ul>
+						{posts.length === 0 ? (
+							<div className="text-center">
+								<p>記事がありません。</p>
+							</div>
+						) : (
+							<Posts posts={posts} />
+						)}
+					</section>
+				</Container>
+			</div>
+		</>
 	);
 }
