6213fe2d1f8e137874c410c73bacf4867b12847d
進藤雄太朗 <nihonshishinyu1994@gmail.com>
2024-03-12 20:18:01 +0900

    fix:ハンバーガーメニューアクセシビリティ

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Nav() {
 	const [navIsOpen, setNavIsOpen] = useState(false);
@@ -9,6 +9,19 @@ export default function Nav() {
 	const closeNav = () => {
 		setNavIsOpen(false);
 	};
+	useEffect(() => {
+		const handleKeyDown = (e) => {
+			if (e.key === "Escape") {
+				setNavIsOpen(false);
+			}
+		};
+
+		document.addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
 	return (
 		<nav
 			className={
@@ -33,11 +46,17 @@ export default function Nav() {
 			<button
 				className="bofore:content-[''] relative z-10 grid h-[40px] w-[32px] place-items-center  before:absolute before:left-0 before:top-[18px] before:z-10 before:h-[2px] before:w-[32px] before:bg-black after:absolute after:left-0 after:top-[20px] after:z-10 after:h-[2px]  after:w-[32px] after:bg-black after:content-[''] dark:before:bg-white dark:after:bg-white md:hidden"
 				onClick={toggleNav}
+				area-expanded={navIsOpen ? "true" : "false"}
+				area-controls="nav"
 			>
 				<span className="absolute left-0 h-[2px] w-[32px] bg-black dark:bg-white"></span>
 				<span className="sr-only">MENU</span>
 			</button>
-			<ul className="fixed inset-[0_-100%] z-[-1] grid  place-content-center  gap-8 bg-black/60 text-center transition-transform duration-300 ease-out md:static md:mr-8 md:flex md:gap-10  md:bg-inherit">
+			<ul
+				className="fixed inset-[0_-100%] z-[-1] grid  place-content-center  gap-8 bg-black/60 text-center transition-transform duration-300 ease-out md:static md:mr-8 md:flex md:gap-10  md:bg-inherit"
+				area-hidden={navIsOpen ? "false" : "true"}
+				id="nav"
+			>
 				<li>
 					<Link
 						href="/me/"
